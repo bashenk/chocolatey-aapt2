56500939dd009dcefdb6ffcba92864e7c8b6471f
Brian Shenk <40068887+bashenk@users.noreply.github.com>
2019-11-05 18:29:16 -0500

    Removed beforemodify and uninstall ps1 files

--- tools/chocolateyuninstall.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop';
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'aapt2*'
-  fileType      = 'EXE'
-  silentArgs   = ''
-  validExitCodes= @(0)
-}
-$uninstalled = $false
